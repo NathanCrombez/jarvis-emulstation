6a7b86cfb941776ceecd743a24032e80c0a655de
NathanCrombez <nathan.crombez@gmail.com>
2017-07-13 21:36:00 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -3,8 +3,7 @@
 # You can also use here user variables defined in your config file
 jv_plugin_emulstation_launch (){
     export DISPLAY=":0.0";
-    /dev/null >1 /dev/null;
-    echo  ${EmulStationPath}emulstation ; 
-    ${EmulStationPath}emulstation& 
+    echo  ${EmulStationPath}emulationstation ; 
+    ${EmulStationPath}emulationstation& 
 }
 
