288ac6eae6f8a81605424d7898033b5b62249321
NathanCrombez <nathan.crombez@gmail.com>
2017-07-13 20:51:51 +0200

    say smthg

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
-
-
-jv_emulstation_PressKey(){
-    #WID=`xdotool search --name "Calculatrice" | head -1`
-    #xdotool windowfocus $WID
-    #xdotool key --clearmodifiers $1
+#WID=`xdotool search --name "Calculatrice" | head -1`
+#xdotool windowfocus $WID
+#xdotool key --clearmodifiers $1
+jv_emulstation_presskey(){
+    echo "ok";
 }
 
 
