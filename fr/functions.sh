d2eebecca39d45fdf9739bc7e9cbe7032d571f75
NathanCrombez <nathan.crombez@gmail.com>
2017-07-13 21:33:10 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -2,8 +2,9 @@
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
 jv_plugin_emulstation_launch (){
-    export DISPLAY=":0.0"
-    killall chromium-browser >& /dev/null >1 /dev/null 
-    chromium-browser & 
+    export DISPLAY=":0.0";
+    /dev/null >1 /dev/null;
+    echo  ${EmulStationPath}emulstation ; 
+    ${EmulStationPath}emulstation& 
 }
 
