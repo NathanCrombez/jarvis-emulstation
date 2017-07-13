451b83940f3084c7c58719793ae6598d203daa8d
NathanCrombez <nathan.crombez@gmail.com>
2017-07-13 21:25:43 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
-
+jv_plugin_emulstation_launch (){
+    chromium-browser & 
+}
 
