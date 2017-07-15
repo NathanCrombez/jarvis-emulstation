438bd32709adcbe5e58393c2a536e8357aa152e2
NathanCrombez <nathan.crombez@gmail.com>
2017-07-15 15:03:24 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -9,13 +9,12 @@ jv_plugin_emulstation_launch (){
 jv_plugin_emulstation_game(){
     export DISPLAY=":0.0";
     jeu=$1;
-    database="/home/pi/.emulationstation/gamelist.db"; 
-    echo "Recherche du jeu $jeu dans la database : $database";
-    echo 'select name from files where name like "%'$jeu'%"';
+    database=${EmulStationConfigPath}"gamelist.db"; 
+    #echo "Recherche du jeu $jeu dans la database : $database";
     rom_fileid=`sqlite3 $database 'select fileid from files where name like "%'$jeu'%"'`
     rom_systemid=`sqlite3 $database 'select systemid from files where name like "%'$jeu'%"'`
-    cmd_emul=`xml_grep --text_only --cond "name" --cond "command" /home/pi/.emulationstation/es_systems.cfg`
-    path_emul=`xml_grep --text_only --cond "name" --cond "path" /home/pi/.emulationstation/es_systems.cfg`
+    cmd_emul=`xml_grep --text_only --cond "name" --cond "command" "'${EmulStationConfigPath}'"es_systems.cfg`
+    path_emul=`xml_grep --text_only --cond "name" --cond "path" "'${EmulStationConfigPath}'"es_systems.cfg`
     
     cmd_emul=`echo $cmd_emul |  grep -oP "(?<="$rom_systemid" )[^%ROM%]+"`
     path_emul=`echo $path_emul |  grep -oP "(?<="$rom_systemid" )[^ ]+"`
