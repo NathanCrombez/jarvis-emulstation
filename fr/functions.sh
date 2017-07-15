b16a87eafd1aec3f666dd33719d0b4030c4eae55
NathanCrombez <nathan.crombez@gmail.com>
2017-07-15 12:26:52 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -13,11 +13,13 @@ jv_plugin_emulstation_game(){
     echo 'select name from files where name like "%'$jeu'%"';
     rom_name=`sqlite3 $database 'select name from files where name like "%'$jeu'%"'`
     rom_systemid=`sqlite3 $database 'select systemid from files where name like "%'$jeu'%"'`
-    rom_path=`sqlite3 $database 'select path from files where name like "%'$jeu'%"'`
     cmd_emul=`xml_grep --text_only --cond "name" --cond "command" /home/pi/.emulationstation/es_systems.cfg`
+    path_emul=`xml_grep --text_only --cond "name" --cond "path" /home/pi/.emulationstation/es_systems.cfg`
+    
     cmd_emul=`echo $cmd_emul |  grep -oP "(?<=snes )[^(%ROM%)]+"`
+    path_emul=`echo $path_emul |  grep -oP "(?<=snes )[^ ]+"`
 
-    echo $cmd_emul $rom_path"/"$rom_name".*"
-    $cmd_emul $rom_path"/"$rom_name".*"
+    echo $cmd_emul $path_emul"/"$rom_name".*"
+    $cmd_emul $path_emul"/"$rom_name".*"
 }
 
