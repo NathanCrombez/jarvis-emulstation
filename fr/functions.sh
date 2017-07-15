6f5fc98b36eb1cc6ca9066248f33adf30d2f5e04
NathanCrombez <nathan.crombez@gmail.com>
2017-07-15 12:29:57 +0200

    ok

--- fr/functions.sh
+++ fr/functions.sh
@@ -16,7 +16,7 @@ jv_plugin_emulstation_game(){
     cmd_emul=`xml_grep --text_only --cond "name" --cond "command" /home/pi/.emulationstation/es_systems.cfg`
     path_emul=`xml_grep --text_only --cond "name" --cond "path" /home/pi/.emulationstation/es_systems.cfg`
     
-    cmd_emul=`echo $cmd_emul |  grep -oP "(?<=snes )[^(%ROM%)]+"`
+    cmd_emul=`echo $cmd_emul |  grep -oP "(?<=snes )[^%ROM%]+"`
     path_emul=`echo $path_emul |  grep -oP "(?<=snes )[^ ]+"`
 
     echo $cmd_emul $path_emul"/"$rom_name".*"
